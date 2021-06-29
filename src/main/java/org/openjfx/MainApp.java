3a622193c083c0d82c9dec05cbb60f3dcfdcd928
ErikDokken2 <dokkenboy@knights.ucf.edu>
2021-06-29 19:52:50 -0400

    Initial commit

---
+++ src/main/java/org/openjfx/MainApp.java
@@ -0,0 +1,42 @@
+package org.openjfx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javax.imageio.IIOException;
+import java.io.IOException;
+
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+
+            stage.setTitle("JavaFX and Gradle");
+            stage.setScene(scene);
+            stage.show();
+        }catch(IOException e){
+                e.printStackTrace();
+        }
+
+//        Scene scene = new Scene(root);
+//        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
+//
+//        stage.setTitle("JavaFX and Gradle");
+//        stage.setScene(scene);
+//        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
